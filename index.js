db631eb9eda0480e8048d418d5018b252f1a2b20
andyradz <and.radz@wp.pl>
2021-08-10 11:49:59 +0200

    1 binaries uploaded

--- index.js
+++ index.js
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom';
 
 import App from './app';
 import Licznik from './useLocalStorageState';
+import ProductInfo from './productInfo';
 
 ReactDOM.render(<App />, document.querySelector('#root'));
 ReactDOM.render(<Licznik />, document.querySelector('#root'));
+ReactDOM.render(<ProductInfo />, document.querySelector('#root'));
